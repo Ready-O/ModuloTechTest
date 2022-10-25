34cd6c9b034fcf225898bff05402dc7d1e3c7aeb
Ready-O <r.aoutem@gmail.com>
2022-10-25 12:34:40 +0200

    Setup Hilt for DI

---
+++ app/src/main/java/com/reda/modulotechtest/App.kt
@@ -0,0 +1,11 @@
+package com.reda.modulotechtest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
